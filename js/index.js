215f154e4dc58d8dd76f772c190bc532f7d42653
Malik Ebers <malik.ebers@web.de>
2020-04-27 17:48:43 +0200

    Added a on-scroll fade animation

--- js/index.js
+++ js/index.js
@@ -18,6 +18,31 @@ $(function() {
   });
 });
 
+// Fade-in on scroll
+
+const fader = document.querySelectorAll('.fade-in');
+
+const options = {
+  threshold: 0.5,
+  rootMargin: "0px 0px -100px 0px"
+};
+
+const fadeOnScroll = new IntersectionObserver(function(
+  entries, fadeOnScroll) {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      fadeOnScroll.unobserve(entry.target);
+    }
+  });
+  }, options);
+
+  fader.forEach(fader => {
+    fadeOnScroll.observe(fader);
+  })
+
 // Homepage SVG
 
 const logo = document.querySelectorAll('#logo path');
